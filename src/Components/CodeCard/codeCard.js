ed238a435471d11e03558f90c61c3d7c2f602c57
Maxwell Dalboni <dalboni.max@hotmail.com>
2022-04-06 22:50:22 -0300

    Testing import change

--- src/Components/CodeCard/codeCard.js
+++ src/Components/CodeCard/codeCard.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import darcula from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import './codeCard.css';
 
 function UserCodePythonCard({ varname, name, nationality, livingIn, birthYear, spokenLangs, progLangs, databases, wishes, optional = '' }) {
